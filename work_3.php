a8140b3135b434266de7f8a848470f226e06633c
NiktiaGordeev <adibilkova@mail.ru>
2025-03-05 14:40:15 +0500

    Delete work_3.php

--- work_3.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-function countWords(string $stroka): int {
-    return count(preg_split('/\s+/', trim($stroka)));
-}
-
-$stroka = "Съешь ещё этих мягких французских булок, да выпей же чаю.";
-
-print(countWords($stroka));
